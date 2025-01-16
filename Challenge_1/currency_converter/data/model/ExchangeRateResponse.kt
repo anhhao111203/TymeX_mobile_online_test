3675b14d928dabffab04cf3dc715c96df53373cb
Anh Hào <118757132+anhhao111203@users.noreply.github.com>
2025-01-16 16:19:52 +0700

    Delete Challenge_1/currency_converter directory

--- Challenge_1/currency_converter/data/model/ExchangeRateResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.currency_converter.data.model
-
-data class ExchangeRateResponse(
-    val success: Boolean = false,
-    val timestamp: Long = 0L,
-    val base: String = "",
-    val date: String = "",
-    val rates: Map<String, Double> = emptyMap()
-)
--- Challenge_1/currency_converter/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.currency_converter.utils
-
-object Constants {
-    const val API_KEY = "4e1f39811cd9b175f364634032f3eaf6"
-}
